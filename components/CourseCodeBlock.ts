3c7191834734694154406e51c2e81ff38403fdb4
xJAWW <pdjaww@gmail.com>
2026-09-16 01:06:43 +0700

    fix course code and image rendering

--- components/CourseCodeBlock.ts
+++ components/CourseCodeBlock.ts
@@ -10,6 +10,11 @@ export const CourseCodeBlock = Node.create({
   parseHTML: () => [{ tag: 'pre[data-course-code]' }],
   renderHTML: ({ node }) => ['pre', { 'data-course-code': '', 'data-language': node.attrs.language, 'data-runnable': node.attrs.runnable }, ['code', 0]],
   addCommands: () => ({
-    setCourseCode: (attributes = { language: 'javascript', runnable: true }) => ({ commands }: any) => commands.insertContent({ type: 'courseCodeBlock', attrs: attributes, content: [{ type: 'text', text: '' }] })
+    setCourseCode: (attributes = { language: 'javascript', runnable: true }) => ({ state, dispatch }: any) => {
+      const position = state.selection.to
+      const node = state.schema.nodes.courseCodeBlock.create(attributes, state.schema.text('// Tulis kode JavaScript di sini'))
+      if (dispatch) dispatch(state.tr.insert(position, node).setSelection(state.selection.constructor.near(state.tr.doc.resolve(position + 1))))
+      return true
+    }
   })
 })
